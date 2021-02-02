873d3445994c4e728b22314f262600998c80d6f3
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-02-02 15:24:16 +0900

    Modify load logic and extract common caching logic

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const App = () => {
     // 値を保持
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "./esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
 
