2e650d1d378e1ff338e3b74e3b946317e0ba6980
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-02-23 02:28:19 +0900

    Add insertCellBefore action to add-cell.tsx and display it

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
@@ -8,10 +10,18 @@ const CellList: React.FC = () => {
   console.log(cells);
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
