16bada975cd42825a374adacc332875b3ea15427
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-02-22 18:25:50 +0900

    Render the list of cells

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom";
-// import CodeCell from "./components/code-cell";
+import CellList from "./components/cell-list";
 import { Provider } from "react-redux";
 import { store } from "./state";
-import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
-        {/* <CodeCell /> */}
+        <CellList />
       </div>
     </Provider>
   );
