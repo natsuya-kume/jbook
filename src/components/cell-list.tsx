e3dd4b300912b7e7be5d239c784db2a26db86b45
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-03-07 03:11:45 +0900

    Call fetch cell and save cell action
    Add persist middleware to persist list of cells

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,14 +1,21 @@
 import "./cell-list.css";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
+import { useActions } from "../hooks/use-actions";
 
 const CellList: React.FC = () => {
   // 別ファイルで定義したのuseTypedSelectorを使用して、reduxにアクセスし、cellsとして、テキストエディタとコードエディタの情報を取得
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
   );
+
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
 
   // console.log(cells);
 
